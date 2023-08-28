7bae2e67b96a8935249aacaed6945af12513b988
matter <ionq652@gmail.com>
2023-08-28 11:32:50 +0300

    contact-us resp

--- script/contact-us.js
+++ script/contact-us.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         var scrollVision = $(window).scrollTop();
+        var backgroundPosition = "center " + (scrollVision / 2) + "px";
+
         $(".startup").css({
-            "background-size": (1 + scrollVision / 1500) * 100 + "%",
-            "background-position": "center " + (-scrollVision / 100) + "px, " + (scrollVision / 100) + "px center"
+            "background-position": backgroundPosition
         });
     });
 });
